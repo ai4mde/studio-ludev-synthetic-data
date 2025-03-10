c7876c106809335522ca4c8490fd08137c32d8e2
JiaLiang Wu <s3684059@vuw.leidenuniv.nl>
2025-03-10 12:14:05 +0100

    Implemented manual injection of data into prototype database. It works only for simple UML diagram with one class.

--- prototypes/backend/schema_extract.py
+++ prototypes/backend/schema_extract.py
@@ -24,3 +24,39 @@ for model in apps.get_models():
             if field.choices == None:
                 continue
             print(field.name, "choices: ", field.choices)
+
+
+for model in apps.get_models():
+    if model.__name__ not in hidden_models:
+        print(f"Injecting data into model: {model.__name__}")
+
+        instance = model()
+
+        for field in model._meta.fields:
+            field_name = field.name
+            
+            if isinstance(field, django.db.models.fields.AutoField): #skip primary key
+                continue
+
+            try:
+                if isinstance(field, django.db.models.CharField):
+                    setattr(instance, field_name, "testdata")
+                elif isinstance(field, django.db.models.IntegerField):
+                    setattr(instance, field_name, 123)
+                elif isinstance(field, django.db.models.FloatField):
+                    setattr(instance, field_name, 123.45)
+                elif isinstance(field, django.db.models.BooleanField):
+                    setattr(instance, field_name, True)
+                elif isinstance(field, django.db.models.TextField):
+                    setattr(instance, field_name, "testdata testdata testdata testdata")
+                elif isinstance(field, django.db.models.ForeignKey):
+                    continue  #skipping foreignley for now, need to be implemented in the future
+                
+            except Exception as e:
+                print(f"Skipping field '{field_name}' due to error: {e}")
+
+        try:
+            instance.save()
+            print(f"Data injected into {model.__name__} successfully!")
+        except Exception as e:
+            print(f"Failed to save data for {model.__name__}: {e}")
